82604ae49aa78e174f4e14f59d1e919ac3228195
Daniel Diaz <danieldiazrendon@hotmail.com>
2023-05-25 04:26:40 +0100

    Segunda pre entrega de actividad

--- index.js
+++ index.js
@@ -1,36 +1,62 @@
-/** Declarar variables */
-const nombres = [];
-const apellidos = [];
-const nombresCompletos = [];
-
-/** Seteo de controlador para ciclo while */
-let controlador = false;
-
-/** Ciclo while para empezar a agregar informacion en el client side */
-while ( !controlador ) {
-  let nombre = prompt(`Ingrese el nombre en orden, si desea parar, no ingrese informacion`);
-  if ( nombre == `` ) 
-    controlador = true;
-  else
-    nombres.push(nombre)
+function addItem () {
+  let element = document.getElementById("addItemForm");
+  let hidden = element.getAttribute("hidden");
+  let button = document.getElementById("addItemButton");
+  if (hidden) {
+    element.removeAttribute("hidden");
+    button.innerText = "Cancelar";
+    document.getElementById("nombre").value = '';
+    document.getElementById("descripcion").value = '';
+    document.getElementById("precio").value = 0;
+ } else {
+    element.setAttribute("hidden", "hidden");
+    button.innerText = "Agregar Elemento";
+ }
 }
 
-/** Ciclo for con do while para ingresar apellidos de cada entrada */
-for ( i = 0; i < nombres.length; i++ ) {
-  let apellido = ``;
-  do {
-    apellido = prompt(`Ingrese los apellidos en orden`)
-    apellidos.push(apellido);
-  } while (apellido != `` && nombres.length > apellidos.length);
-}
+function aceptar() {
+  let nombre = document.getElementById("nombre").value;
+  let descripcion = document.getElementById("descripcion").value;
+  let precio = document.getElementById("precio").value;
+  let element = document.getElementById("addItemForm");
+  let button = document.getElementById("addItemButton");
 
-/** Funcion para concatenar nombres completos */
-const concatenarNombreCompleto = function() {
-  for ( i = 0; i < nombres.length; i++ ) {
-    nombresCompletos.push(`${nombres[i]} ${apellidos[i]}`);
-    alert(`${i+1}.- ${nombresCompletos[i]}`);
+  if ( nombre == '' || descripcion == '' || precio == '' ) 
+    return alert ('Llena todos los campos');
+
+  if ( items.some(x => x.nombre == nombre) )
+    return alert ('Elemento ya agregado');
+
+  items.push({
+    nombre: nombre,
+    descripcion: descripcion,
+    precio: precio
+  });
+
+  document.getElementById("nombre").value = '';
+  document.getElementById("descripcion").value = '';
+  document.getElementById("precio").value = 0;
+
+  document.getElementById('listaItems').innerText = ''
+
+  let ul = document.createElement('ul');
+
+  for (const item of items){
+    let li = document.createElement('li');
+    li.innerHTML = "Nombre: " + item.nombre;
+    ul.appendChild(li);
+    li = document.createElement('li')
+    li.innerHTML = "Descripcion: " + item.descripcion;
+    ul.appendChild(li);
+    li = document.createElement('li')
+    li.innerHTML = "Precio: " + "$" + item.precio;
+    ul.appendChild(li);
+    let div = document.createElement('div');
   }
+  document.getElementById('listaItems').appendChild(ul);
+  element.setAttribute("hidden", "hidden");
+  button.innerText = "Agregar Elemento";
+
 }
 
-/** Llamado a la funcion */
-concatenarNombreCompleto();
+const items = [];
